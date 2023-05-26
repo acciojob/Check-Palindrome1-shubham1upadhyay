7a211e09cc539c0de0e12aea26d9ca3d4d7966cf
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-26 16:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-// complete the given function
+function callme()
+{
+    var str = document.getElementById('name').value;
 
-function palindrome(str){
-  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-  let i = 0, j = str.length - 1;
-  while (i < j) {
-    if (str[i] !== str[j])
-	{
-      return false;
+    str = str.toLowerCase().replace(" ", '');
+    let bag = "";
+    for(let i=str.length-1; i>=0; i--)
+    {
+        bag += str.charAt(i);
     }
-    i++;
-    j--;
+
+    if(str.trim() === bag.trim())
+    {
+        document.getElementById('result').innerHTML = "true";
+    }else{
+        document.getElementById('result').innerHTML = "false";
+    }
   }
-  
-  return true;
-}
-module.exports = palindrome

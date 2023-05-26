213ed6b6bb803598c352c0947a1baabaefcc2753
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-26 16:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function callme()
 {
     var str = document.getElementById('name').value;
 
-    str = str.toLowerCase().replace(" ", '');
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
     let bag = "";
     for(let i=str.length-1; i>=0; i--)
     {
         bag += str.charAt(i);
-    }
+	}
 
     if(str.trim() === bag.trim())
     {
